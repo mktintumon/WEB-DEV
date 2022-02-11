885a224be13a703c831a7e2adfcf47427e0b6fe5
mktintumon <mktintumon@gmail.com>
2022-02-11 14:37:11 +0530

    added

--- 7.Web Scraping/espnScraper/main.js
+++ 7.Web Scraping/espnScraper/main.js
@@ -3,6 +3,16 @@ const url = 'https://www.espncricinfo.com/series/ipl-2020-21-1210595'
 const request = require('request')
 const cheerio = require('cheerio')
 const getLinkObj = require('./allMatchLink')
+const path = require('path')
+const fs = require('fs')
+
+
+let iplPath = path.join(__dirname , "IPL")
+function dirCreator(filepath){
+    if(fs.existsSync(filepath) == true){
+          fs.mkdirSync()
+    }
+}
 
 request(url , cb)
 
