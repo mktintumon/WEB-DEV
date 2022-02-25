a4854d9b9d6da1527f084b5cc8b9c885bfc5e349
mktintumon <mktintumon@gmail.com>
2022-02-25 13:11:06 +0530

    added

--- 10_Automation/hr.js
+++ 10_Automation/hr.js
@@ -1,6 +1,7 @@
 const loginLink = "https://www.hackerrank.com/auth/login";
 
 let puppeteer = require('puppeteer')
+let codeFile = require('./code')
 
 let email = 'kebegib194@diolang.com'
 let password = 'Mohit@123'
@@ -38,7 +39,56 @@ launchBrowser.then(function(browserInstance){
 }).then(function(){
     let loginPromise = page.click('button[data-analytics="LoginPassword"]', {delay: 100,})
     return loginPromise
+
+}).then(function(){
+
+    let algoWillBeClickedPromise = waitAndClick('.topic-card a[data-attr1="algorithms"]' , page)
+    return algoWillBeClickedPromise
+
+}).then(function(){
+
+    let getToWarmupPromise = waitAndClick('input[value="warmup"]', page)
+    return getToWarmupPromise
+
+}).then(function(){
+
+    let challengesArrPromise = page.$$(".ui-btn.ui-btn-normal.primary-cta.ui-btn-line-primary.ui-btn-styled",
+    { delay: 100 })
+    return challengesArrPromise
+
+}).then(function(questionArr){
+    console.log("No. of questions ->" + questionArr.length)
+
+    let questionWillBeSolvedpromise = questionSolver(page , questionArr[0] , codeFile.answers[0] )
 })
 
 
 
+function waitAndClick(selector , currPage){
+   return new Promise(function(resolve , reject){
+       let waitForModalPromise = currPage.waitForSelector(selector)
+
+       waitForModalPromise
+            .then(function(){
+                let clickModalPromise = currPage.click(selector , {delay : 100})
+                return clickModalPromise
+            })
+            .then(function(){
+                resolve()
+            })
+            .catch(function(){
+                reject()
+            })
+    })
+}
+
+
+function questionSolver(page , question , answer){
+    return new Promise(function(resolve , reject){
+      
+        let questionWillBeClickedPromise = question.click()
+        questionWillBeClickedPromise.then(function(){
+            console.log("done")
+        })
+    })
+}
