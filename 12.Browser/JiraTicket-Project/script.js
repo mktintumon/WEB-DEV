c0da40bae226519c7e44eb2cacb1048aba69f4aa
mktintumon <mktintumon@gmail.com>
2022-03-23 23:52:51 +0530

    added

--- 12.Browser/JiraTicket-Project/script.js
+++ 12.Browser/JiraTicket-Project/script.js
@@ -1,5 +1,27 @@
 let addBtn = document.querySelector('.add-btn')
 
-addBtn.addEventListener("click" , function(e){
-    console.log('added bro')
+let modalCont = document.querySelector('.modal-cont')
+
+let addFlag = false
+
+addBtn.addEventListener('click' , function(e){
+        //Display the Modal
+
+        // addFlag -> true -> Modal Display
+        //addFlag -> false -> Modal Hide
+
+  addFlag = !addFlag
+
+ if(addFlag==true){
+   modalCont.style.display = 'flex'
+ }
+
+ else{
+       modalCont.style.display = 'none'
+ }
+
+
+
+
+
 })
