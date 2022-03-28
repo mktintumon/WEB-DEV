ff0495d4ebcad9873c4cd6bb042b65da83bd95ec
mktintumon <mktintumon@gmail.com>
2022-03-28 17:13:31 +0530

    added

--- 12.Browser/Advanced_Todo_App/script.js
+++ 12.Browser/Advanced_Todo_App/script.js
@@ -1,48 +1,91 @@
-let addBtn = document.querySelector('.add-btn')
+let addBtn = document.querySelector(".add-btn")
+let addFlag = false;
 
-let modalCont = document.querySelector('.modal-cont')
+let removeBtn = document.querySelector(".remove-btn")
+let removeFlag = false
 
-let mainCont = document.querySelector('.main-cont')
+let modalCont = document.querySelector(".modal-cont")
 
-let addFlag = false
+let mainCont = document.querySelector(".main-cont")
+
+let colors = ["lightpink", "lightblue", "lightgreen", "black"]
+let modalPriorityColor = colors[colors.length - 1]; // set to black
+
+let allPriorityColors = document.querySelectorAll(".priority-color");
+
+let taskAreaCont = document.querySelector('.textarea-cont')
 
 
-// Display modal container
-addBtn.addEventListener('click' , function(e){
+//Display the Modal
+addBtn.addEventListener("click", function (e) {
 
-//  addFlag -> true -> Modal Display
-//  addFlag -> false -> Modal Hide
+  // addFlag , true - Modal Display
+  //addFlag , false - Modal Hide
 
-addFlag = !addFlag
+  addFlag = !addFlag;
 
-if(addFlag==true){
-    modalCont.style.display = 'flex'
+  if (addFlag == true) {
+    modalCont.style.display = "flex";
+  } else {
+    modalCont.style.display = "none";
+  }
+});
+
+
+//Changing Priority Colors
+allPriorityColors.forEach(function (colorElem) {
+  colorElem.addEventListener("click", function (e) {
+    allPriorityColors.forEach(function (priorityColorElem) {
+      priorityColorElem.classList.remove("active");
+    });
+    colorElem.classList.add("active");
+
+    modalPriorityColor = colorElem.classList[0];
+  });
+});
+
+
+// Generating a Ticket
+modalCont.addEventListener("keydown", function (e) {
+  let key = e.key;
+
+  if (key == "Shift") {
+    createTicket(modalPriorityColor , taskAreaCont.value); 
+    modalCont.style.display = "none";
+    addFlag = false;
+    taskAreaCont.value = ''
+  }
+});
+
+function createTicket(ticketColorClass , task) {
+  let ticketCont = document.createElement("div");
+  ticketCont.setAttribute("class", "ticket-cont");
+
+  ticketCont.innerHTML = `<div class="ticket-color ${ticketColorClass} "></div>
+  <div class="ticket-id"></div>
+  <div class="task-area">${task}</div>`;
+
+  mainCont.appendChild(ticketCont);
+
+  handleRemove(ticketCont)
 }
 
-else{
-    modalCont.style.display = 'none'
-}
 
-})
+removeBtn.addEventListener('click' , function(e){
+      removeFlag = !removeFlag
 
-
-// creating ticket
-modalCont.addEventListener('keydown' , function(e){
-      let key = e.key
-
-      if(key == 'Shift'){
-            createTicket()
-            modalCont.style.display = 'none'
+      if(removeFlag == true){
+         removeBtn.style.color = 'red'
+      }
+      else{
+         removeBtn.style.color = 'white'
       }
 })
 
-function createTicket(){
-      let ticketCont = document.createElement('div')
-      ticketCont.setAttribute('class' , 'ticket-cont')
-
-      ticketCont.innerHTML = ` <div class="ticket-color"></div>
-      <div class="ticket-id"></div>
-      <div class="task-area"></div>`
-
-      mainCont.appendChild(ticketCont)
+function handleRemove(ticket){
+      ticket.addEventListener('click' , function(e){
+            if(removeFlag == true){
+                  ticket.remove()
+            }
+      })
 }
