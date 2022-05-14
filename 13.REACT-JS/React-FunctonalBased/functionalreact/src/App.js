9d3693866ea623897a2a5630c7bdbeb86feb49a2
mktintumon <mktintumon@gmail.com>
2022-05-14 23:35:58 +0530

    added

--- 13.REACT-JS/React-FunctonalBased/functionalreact/src/App.js
+++ 13.REACT-JS/React-FunctonalBased/functionalreact/src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import Test from './components/Test';
 import Todo from './components/Todo';
 import UseEff3 from './components/UseEff3';
+import Text from './components/Test'
 
 function App() {
   return (
     <div>
-     {/* <UseEffect1/> */}
-     <UseEff3/>
+     {/* <UseEff3/> */}
+     <Text/>
     </div>
   );
 }
