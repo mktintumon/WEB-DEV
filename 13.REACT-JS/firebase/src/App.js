cdbab5cac22b8136671869320bdf0a6e62bd584b
mktintumon <mktintumon@gmail.com>
2022-05-21 12:26:49 +0530

    added

--- 13.REACT-JS/firebase/src/App.js
+++ 13.REACT-JS/firebase/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Fireauth from './Components/Fireauth';
 import Firestore from './Components/Firestore';
+import Firestorage from './Components/Firestorage';
 
 function App() {
   return (
-    // <Fireauth/>
-    <Firestore/>
+    <>
+    <Fireauth/>
+    <Firestorage/>
+    </>
   );
 }
 
---
+++ 13.REACT-JS/firebase/src/Components/Firestorage.js
@@ -0,0 +1,41 @@
+import React,{useState} from 'react'
+import {storage} from '../firebaseDemo';
+
+function Firestorage() {
+    const [file,setFile] = useState('');
+
+    const upload = () => {
+        console.log(file)
+        const uploadTask = storage.ref(`/data/${file.name}`).put(file);
+        uploadTask.on('state_changed',fn1,fn2,fn3);
+
+        // work in progress
+        function fn1(snapshot){
+            let progress = (snapshot.bytesTransferred / snapshot.totalBytes)*100;
+            console.log(`Upload is ${progress} done.`)
+        }
+
+        //error occured
+        function fn2(error){
+            console.log('error',error)
+        }
+
+        // successful
+        function fn3(){
+            uploadTask.snapshot.ref.getDownloadURL().then((url)=>{
+                console.log(url);
+            })
+        }
+
+    }
+    
+    return (
+        <div>
+            <label htmlFor="file">File:</label>
+            <input type="file" accept="image/*" onChange={(e)=>setFile(e.target.files[0])}/>
+            <button onClick={upload}>Upload</button>
+        </div>
+    )
+}
+
+export default Firestorage
