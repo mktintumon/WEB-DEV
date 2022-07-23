7a2aa9c597ba5c88fbd73f10c8e0ecee00592ec3
mktintumon <mktintumon@gmail.com>
2022-07-23 19:37:31 +0530

    upto mongoose

--- 15.Backend/MongoDB/mongoose.js
+++ 15.Backend/MongoDB/mongoose.js
@@ -14,45 +14,62 @@ const mongoose = require('mongoose');
 
 let dbLink = 'mongodb+srv://mktintumon:<password>@cluster0.krtav.mongodb.net/?retryWrites=true&w=majority'
 mongoose
-  .connect(dbLink)
-  .then(()=>{
-     console.log('DB connection established');
-   })
-   .catch((error)=>{
-     console.log('Error' , error.message);
-   })
+    .connect(dbLink)
+    .then(() => {
+        console.log('DB connection established');
+    })
+    .catch((error) => {
+        console.log('Error', error.message);
+    })
+
+
 
 // Creating SCHEMA---> only entries written will be added to your db AND nothing else will be added 
 let userSchema = mongoose.Schema({
-    name : {
-        type : String,
-        required : true,
+    name: {
+        type: String,
+        required: [true, "Name is required"],
     },
-    email :{
-        type : String,
-        required : true,
+    email: {
+        type: String,
+        required: [true, "Email is required"],
     },
-    password :{
-        type : String,
-        required : true,
+    password: {
+        type: String,
+        required: [true, "Password is required"],
     },
-    confirmPassword :{
-        type : String,
-        required : true,
+    confirmPassword: {
+        type: String,
+        required: [true, "ConfirmPassword is required"],
+
+        // custom validator
+        validate: {
+            validator: function () {
+                // this referes to the current entry 
+                return this.password == this.confirmPassword;
+            },
+            //error message
+            message: "password miss-match"
+        },
     },
-    phoneNumber :{
-        type : String,
-        minLength : 10,
-        maxLength : 10,
+    phoneNumber: {
+        type: String,
+        minLength: [10, "Less than 10"],
+        maxLength: [10, "More than 10"],
     },
-    pic :{
-        type : String,
-        default : 'mohit1.jpg',
+    pic: {
+        type: String,
+        default: 'mohit1.jpg',
     },
-    address :{
-         type : String,
-    }
-})   
+    address: {
+        type: String,
+    },
+    days: {
+        type: String,
+        enum: ["Mon", "Tue", "Wed"]
+    },
+})
+
 
 // model is similar to your collection 
 // name of the collection, the set of rules this collection should follow
